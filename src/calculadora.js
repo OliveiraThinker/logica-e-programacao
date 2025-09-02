38fa87d91e74bd9ef89c791c36344190881ec64f
magic <72053036+OliveiraThinker@users.noreply.github.com>
2025-09-02 07:30:19 -0400

    Adicionando a média ao exercício proposto.

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,15 +1,15 @@
-//exercício 1 - Crie uma função que receba dois números como parâmetros e retorne a soma desses números.
+//exercício 1 - Crie uma função (verbo) que receba dois números como parâmetros (elementos) e retorne a soma desses números.
 function somarDoisNumeros(valor1, valor2) {
     const resultado = valor1 + valor2;
     return resultado;
 }
-//exercício 2 - calcule a média de dois números utilizando a função do exercício 1.
+//passo 1 - calcule a média de dois números utilizando a função do exercício 1.
 function calcularMediaDeDoisNumeros(valor1, valor2) {
-    const resultadoSomaDeDoisValores = somarDoisNumeros(valor1, valor2);
-    const resultadoDaMediaDeDoisValores = resultadoSomaDeDoisValores / 2;
+    const resultadoDASomaDeDoisValores = somarDoisNumeros(valor1, valor2);
+    const resultadoDaMediaDeDoisValores = resultadoDASomaDeDoisValores / 2;
     return resultadoDaMediaDeDoisValores;
 }
-//exercício 3 - Exporte as funções criadas nos exercícios anteriores.
+//passo 2 - Exporte as funções criadas nos exercícios anteriores.
 module.exports = {
     somarDoisNumeros,
     calcularMediaDeDoisNumeros
--- tests/calculadora.teste.js
+++ tests/calculadora.teste.js
@@ -1,4 +1,6 @@
-//exercício 4 - chamar a função do folder calculadora.js e imprimir o resultado no console. 
-const{somarDoisNumeros} = require('../src/calculadora');
+//passo 3 - chamar/importar as funções do folder calculadora.js para o folder calculadora.teste.js e imprimir o resultado no console/terminal. 
+const{somarDoisNumeros, calcularMediaDeDoisNumeros} = require('../src/calculadora');
 const resultadoDaSoma= somarDoisNumeros(5, 3);
+const resultadoDaMedia= calcularMediaDeDoisNumeros(5, 3);
+console.log(resultadoDaMedia);
 console.log(resultadoDaSoma);
