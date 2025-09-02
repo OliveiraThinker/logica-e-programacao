537b1dc326eb3f23d038d944ef94312726efb6dd
magic <72053036+OliveiraThinker@users.noreply.github.com>
2025-09-02 07:58:11 -0400

    Adicionando a média ao exercício proposto.

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,15 +1,17 @@
-//exercício 1 - Crie uma função (verbo) que receba dois números como parâmetros (elementos) e retorne a soma desses números.
+//exercício 1 - Crie duas funções (verbo) que recebam dois números como parâmetros/variáveis (elementos) e retorne a soma e a média desses números.
+
+//passo 1 - Criando uma função para "somarDoisNumeros"
 function somarDoisNumeros(valor1, valor2) {
     const resultado = valor1 + valor2;
     return resultado;
 }
-//passo 1 - calcule a média de dois números utilizando a função do exercício 1.
+//passo 2 - Criando uma função para "calcularMédiaDeDoisNúmeros" utilizando os valores do passo 1 e apresentando os seus resultados.
 function calcularMediaDeDoisNumeros(valor1, valor2) {
     const resultadoDaSomaDeDoisValores = somarDoisNumeros(valor1, valor2);
     const resultadoDaMediaDeDoisValores = resultadoDaSomaDeDoisValores / 2;
     return resultadoDaMediaDeDoisValores;
 }
-//passo 2 - Exporte as funções criadas nos exercícios anteriores.
+//passo 3 - Exportando as funções criadas nos passos anteriores com o "objeto" que representa o módulo atual "modulo.exports" e suas propriedades "somarDoisNumeros, CalcularMediaDeDoisNumeros".
 module.exports = {
     somarDoisNumeros,
     calcularMediaDeDoisNumeros
--- tests/calculadora.teste.js
+++ tests/calculadora.teste.js
@@ -1,6 +1,6 @@
-//passo 3 - chamar/importar as funções do folder calculadora.js para o folder calculadora.teste.js e imprimir o resultado no console/terminal. 
+//passo 4 - chamar/importar as funções do folder/pasta/diretório "calculadora.js" para o folder "calculadora.teste.js" e imprimir os resultados no console/terminal. 
 const{somarDoisNumeros, calcularMediaDeDoisNumeros} = require('../src/calculadora');
-const resultadoDaSoma= somarDoisNumeros(5, 3);
-const resultadoDaMedia= calcularMediaDeDoisNumeros(5, 3);
-console.log(resultadoDaMedia);
-console.log(resultadoDaSoma);
+const resultadoDaSoma= somarDoisNumeros(10, 8);
+const resultadoDaMedia= calcularMediaDeDoisNumeros(10, 8); 
+console.log(resultadoDaSoma); // saída = 18
+console.log(resultadoDaMedia); // saída = 9
